2358eeb4eaa8b23f054a61c2c7be4c6d2750e6e9
TayannP <tayanperestpz@gmail.com>
2024-05-26 15:34:04 -0300

    revisando sobre context

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { LoggedUserContext } from "@/contexts/LoggedUser"
+import { useContext } from "react"
+
+
+export const Header = () => {
+    const loggedUserCtx= useContext(LoggedUserContext)
+
+    const hangleLogout = () => {
+        loggedUserCtx?.setName('')
+    }
+
+    return (
+        <header>
+            <h1 className="text-3xl">Título da página</h1>
+            {loggedUserCtx?.name && 
+                <>
+                    <p>Usuário Logado: { loggedUserCtx?.name }</p>
+                    <button onClick={hangleLogout}>Sair</button>
+                </>
+            }
+            
+            {(!loggedUserCtx || loggedUserCtx?.name === '') &&
+                <p>Usuário DESLOGADO</p>
+                
+            }
+        </header>
+    )
+}
---
+++ src/contexts/LoggedUser.tsx
@@ -0,0 +1,16 @@
+import { createContext, ReactNode, useState } from "react";
+
+type LoggedUserContextType = {
+    name: string
+    setName: (n: string) => void
+}
+
+export const LoggedUserContext = createContext<LoggedUserContextType | null>(null)
+
+export const LoggedUserProvider = ( { children }: { children: ReactNode}) => {
+    const [name, setName] = useState('joao')
+
+    return (
+        <LoggedUserContext.Provider value={{name, setName}}>{children}</LoggedUserContext.Provider>
+    )
+}
